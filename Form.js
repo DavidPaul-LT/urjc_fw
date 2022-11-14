54dfcfccb2aa55bd26f9e0457e386143a84937d8
DavidPaul-LT <davidpaullimayllat@gmail.com>
2022-11-14 04:00:15 +0100

    Prueba ( crear elemento nvl2 - formulario anclado a elemento (menos al nombre)) 2

---
+++ Form.js
@@ -0,0 +1,61 @@
+
+
+function btnForm(){
+    let divCol = document.createElement("div");
+        let divCard = document.createElement("div");
+            let img = document.createElement("img");
+            let divCardBody = document.createElement("div");
+                let divTextCenter = document.createElement("div");
+                    let h5 = document.createElement("h5");
+                    let divCardFooter = document.createElement("div");
+                        let divTextCenter2 = document.createElement("div");
+                            let a = document.createElement("a");
+
+    divCol.className = "col mb-5";
+    divCol.id = "Borrar";
+    divCard.className = "card h-100";
+    img.className = "card-img-top";
+    img.src = document.getElementById("imgForm").value;
+    divCardBody.className = "card-body p-4";
+    divTextCenter.className = "text-center";
+    h5.className = "fw-bolder";
+    divCardFooter.className = "card-footer p-4 pt-0 border-top-0 bg-transparent";
+    divTextCenter2.className = "text-center";
+    a.className = "btn btn-outline-dark mt-auto";
+
+    let h5Text = document.createTextNode("Flowknit Ultra-Soft Performance Polo");
+    h5.appendChild(h5Text);
+    let aText = document.createTextNode("Add to cart");
+    a.appendChild(aText);
+
+    divCol.appendChild(divCard);
+    divCard.appendChild(img);
+    divCard.appendChild(divCardBody);
+    divCardBody.appendChild(divTextCenter);
+    divTextCenter.appendChild(h5);
+    divTextCenter.appendChild(divCardFooter);
+    divCardFooter.appendChild(divTextCenter2);
+    divTextCenter2.appendChild(a);
+
+    // añade el elemento creado y su contenido al DOM
+    document.getElementById("ContenedorProductostwo").appendChild(divCol);
+}
+
+
+function btnShowForm(){
+    let form = document.getElementById("form");
+    let btnShowForm = document.getElementById("btnShowForm");
+    let btnForm = document.getElementById("btnForm");
+
+
+    if(form.style.display == "none"){
+        form.style.display = "block";
+        btnShowForm.textContent = "Salir";
+        btnForm.style.display = "block";
+    } else {
+        btnShowForm.style.display = 'block';
+        form.style.display = "none";
+        btnShowForm.textContent = "Añadir Producto"
+        btnForm.style.display = "none";
+    }
+}
