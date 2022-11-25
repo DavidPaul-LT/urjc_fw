15f5731fe046f82b803560401be37f690441804a
DavidPaul-LT <davidpaullimayllat@gmail.com>
2022-11-25 14:03:03 +0100

    nada

---
+++ index/js/form,.js
@@ -0,0 +1,20 @@
+
+function btnShowForm(){
+    let form = document.getElementById("form"),
+    btnShowForm = document.getElementById("btnShowForm"),
+    sectionProducts = document.getElementById("sectionProducts"),
+    bannerBF = document.getElementById("bannerBF");
+    
+
+    if(form.style.display == "none"){
+        form.style.display = "block";
+        sectionProducts.style.display = "none";
+        bannerBF.style.display = "none";
+    } else {
+        bannerBF.style.display = "block";
+        btnShowForm.style.display = 'block';
+        form.style.display = "none";
+        btnShowForm.textContent = "+";
+        sectionProducts.style.display = "block";
+    }
+}
