9137d8daa21ada53d586ba8ddbed890f40f21d58
Cristian1A <cav.contact.pr@gmail.com>
2022-11-27 22:18:36 +0100

    Añadida la carpeta con el código modularizado (Subido al main todos los cambios de la rama Cristian) + info ver el anterior commit

---
+++ index/js-modules/Index.js
@@ -0,0 +1,13 @@
+import {Producto} from './Producto.js';
+import {AlmacenProductos} from './AlmacenProductos.js'
+import {Carrito} from './Carrito.js'
+import {Pagina,PaginaPrincipal,PaginaProducto,PaginaCarrito} from './Paginas.js';
+
+
+//---utils
+let storage = new AlmacenProductos();
+let cart = new Carrito();
+
+//---load page
+PaginaPrincipal.mostrar_carrousel();
+PaginaPrincipal.mostrar_almacen_productos(storage);
