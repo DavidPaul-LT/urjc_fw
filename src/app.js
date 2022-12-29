a80631a2112fe4d889188119316edf17c0559551
Cristian1A <cav.contact.pr@gmail.com>
2022-12-29 16:53:47 +0100

    CAMBIOS -Corregido el error al cargar los estilos de la página de producto. -Creados nuevos archivos mustache para estándarizar algunas funcionalidades comunes. -Aumentado el número de productos a 10.

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.engine('mustache', mustacheExpress());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../public/'));
+app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/', storageRouter);
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
--- src/storageRouter.js
+++ src/storageRouter.js
@@ -13,17 +13,17 @@ router.get('/', (req, res) => {
 router.post('/post/new', (req, res) => {
     let {name, image, price, description} = req.body;
     boardService.addPost({name, image, price, description});
-    res.render('saved_post');
+    res.render('saved_product');
 });
 //---gets from -storage- the Product whose -id- is param id
 router.get('/post/:id', (req, res) => {
     let product = storageService.getElement(req.params.id);
-    res.render('show_post', {product});
+    res.render('show_product', {product});
 });
 //---removes from -storage- the Product associated with param id key
 router.get('/post/:id/delete', (req, res) => {
     storageService.removeElement(req.params.id);
-    res.render('deleted_post');
+    res.render('deleted_product');
 });
 
 export default router;
