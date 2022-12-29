6f3d70b7f0ac7ce63418a2d93bcad92e1ba449a4
DavidPaul-LT <davidpaullimayllat@gmail.com>
2022-12-29 21:26:00 +0100

    cambios guardados en rama Paul de Rama Main

--- src/storageRouter.js
+++ src/storageRouter.js
@@ -1,48 +1,29 @@
 import express from 'express';
-import { storage, product } from './productService.js';
-import { storage } from './storageService.js';
+import * as storageService from './storageService.js';
 
 const router = express.Router();
 //---gets from -storage- all the Product instances
-
-
 router.get('/', (req, res) => {
+    let datos = storageService.getStorage();
     res.render('index', { 
-        "list": storage.getElements()
+        storage: datos
     });
 }); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //--- POST from Insertion Form
-router.post('/post/new', (req, res) => {
-    let { user, title, text } = req.body;
-    nuevoProduct = new product;
-    storage.insertElement(nuevoProduct);
-    res.render('saved_post');
+router.post('/product/new', (req, res) => {
+    let {name, image, price, description} = req.body;
+    boardService.addPost({name, image, price, description});
+    res.render('saved_product');
 });
 //---gets from -storage- the Product whose -id- is param id
-router.get('/post/:id', (req, res) => {
-    let product = storageService.storage.getElement(req.params.id);
-    res.render('show_post', { product });
+router.get('/product/:id', (req, res) => {
+    let product = storageService.getElement(req.params.id);
+    res.render('show_product', {product});
 });
 //---removes from -storage- the Product associated with param id key
-router.get('/post/:id/delete', (req, res) => {
-    storageService.storage.deleteElement(req.params.id);
-    res.render('deleted_post');
+router.get('/product/:id/delete', (req, res) => {
+    storageService.removeElement(req.params.id);
+    res.redirect('/');
 });
 
 export default router;
