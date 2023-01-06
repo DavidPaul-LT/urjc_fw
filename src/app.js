d058f132036b54a6bc7fe99ce715994df0c7e184
DavidCCGithubURJC <96200005+DavidCCGithubURJC@users.noreply.github.com>
2023-01-06 11:51:36 +0100

    Formularios aparecen pero no funcionan

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mustacheExpress from 'mustache-express';
+import bodyParser from 'body-parser';
+import { __dirname } from './dirname.js';
+import storageRouter from './storageRouter.js';
+
+const app = express();
+ 
+app.set('views', __dirname + '/../views');
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/../public/'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/', storageRouter);
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
---
+++ src/storageRouter.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import * as storageService from './storageService.js';
+import * as cartService from './cartService.js';
+
+const router = express.Router();
+//---gets from -storage- all the Product instances
+router.get('/', (req, res) => {
+    res.render('index', { 
+        storage: storageService.getStorage().slice(0,5)
+    });
+});
+//---loads more products
+router.get('/loadProducts', (req, res) => {
+    const begin = parseInt(req.query.begin);
+    const to = parseInt(req.query.end);
+    res.render('storage', {
+    storage: storageService.getStorage().slice(begin,to)
+    });
+});
+//--- POST from Insertion Form
+router.post('/productNew', (req, res) => {
+    let {name, image, price, description} = req.body;
+    boardService.addPost({name, image, price, description});
+    res.render('saved_product');
+});
+//---gets from -storage- the Product whose -id- is param id
+router.get('/product/:id', (req, res) => {
+    let product = storageService.getElement(req.params.id);
+    res.render('product_page', {product});
+});
+//---load modificaton form
+router.get('/product/:id/form', (req, res) => {
+    console.log(req.params.id);
+    let subElements = storageService.getSubElements(req.params.id);
+    res.render('form', {subElements});
+});
+//---load empty form
+router.get('/form', (req, res) => {
+    let subElements = storageService.getDefaultSubElements();
+    res.render('form', {subElements});
+});
+//---removes from -storage- the Product associated with param id key
+router.get('/product/:id/delete', (req, res) => {
+    storageService.removeElement(req.params.id);
+    res.render('deleted_product');
+});
+//--- goes to the cart page
+router.get('/cart',(req, res) => {
+    res.render('cart_page',{
+        cart: cartService.getCart()
+    });
+});
+//---insert into -cart- a new product
+router.get('/cart/:id', (req, res) => {
+    let aux = cartService.getElement(req.params.id);
+    if(aux != undefined){
+        let info = storageService.getElement(req.params.id);
+        cartService.insertElement(req.params.id,[info.name, info.price,1]);
+    }else{
+        cartService.insertElement(req.params.id,[aux[0],aux[1],aux[2]+1]);
+    }
+    res.redirect('back');
+});
+
+export default router;
