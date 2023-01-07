5cabc621a09865dcb6f39615bff055c43066cf66
Cristian1A <cav.contact.pr@gmail.com>
2023-01-07 22:09:27 +0100

    Revisión 2, actualización de comentarios, funciones extra carrito, ajustes puntuales y borrado de subelemento aún pendiente

--- src/cartService.js
+++ src/cartService.js
@@ -1,13 +1,13 @@
 //--- cart map contains all the products and its ocurrences which were added to the cart
 let cart = new Map();
-//--- cart format key: product.id value: [name, price, ocurrences]
+//--- cart format key: product.id value: {id, ocurrences}
 
 //--- Returns all the elements contained in -cart- map
 export function getCart(){
     return [...cart.values()]
 }
 //--- Adds a new element into -cart- map
-export function insertElement(id,element){
+export function insertElement(id, element){
     cart.set(id, element);
 }
 //--- Removes an element by its ID from -cart- map
